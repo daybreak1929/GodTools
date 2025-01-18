4e4c1bbfbe7cefc82cee165a04268e18bb36e12f
inmny <1342885270@qq.com>
2025-01-18 23:10:30 +0800

    再调整过滤逻辑使得更合适

--- Code/UI/WindowTops.cs
+++ Code/UI/WindowTops.cs
@@ -156,8 +156,10 @@ public partial class WindowTops : AbstractWideWindow<WindowTops>
             {
                 or_list.AddRange(_list.FindAll(setting.FilterFunc.Invoke));
             }
-
-            _list = _list.Union(or_list).ToList();
+            if (last_and_filter_settings.Count > 0)
+                _list = _list.Intersect(or_list).ToList();
+            else
+                _list = or_list;
         }
         if (last_not_filter_settings.Count > 0)
         {
