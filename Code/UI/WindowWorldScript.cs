88e10e285e7e0ce8ab775d3d8c04d704ffaa5df0
inmny <1342885270@qq.com>
2025-01-23 23:46:17 +0800

    世界脚本的三个框

--- Code/UI/WindowWorldScript.cs
+++ Code/UI/WindowWorldScript.cs
@@ -17,5 +17,26 @@ public class WindowWorldScript : AbstractWideWindow<WindowWorldScript>
         allScriptContent.sizeDelta = new(0, 0);
         allScriptContent.gameObject.AddComponent<ContentSizeFitter>().verticalFit = ContentSizeFitter.FitMode.MinSize;
         allScriptContent.gameObject.AddComponent<VerticalLayoutGroup>().SetSimpleVertLayout();
+
+        var all_script_scroll_view = allScriptContent.parent.parent;
+        var script_configure_scroll_view = Instantiate(all_script_scroll_view, BackgroundTransform);
+        script_configure_scroll_view.name = "ScriptConfigure";
+        var detail_configure_scroll_view = Instantiate(all_script_scroll_view, BackgroundTransform);
+        detail_configure_scroll_view.name = "DetailConfigure";
+
+        all_script_scroll_view.transform.localPosition = new(-220, 0);
+        all_script_scroll_view.GetComponent<RectTransform>().sizeDelta = new(120, 250);
+
+        script_configure_scroll_view.transform.localPosition = new(-70, 0);
+        script_configure_scroll_view.GetComponent<RectTransform>().sizeDelta = new(160, 250);
+
+        detail_configure_scroll_view.transform.localPosition = new(150, 0);
+        detail_configure_scroll_view.GetComponent<RectTransform>().sizeDelta = new(260, 250);
+        
+        
+        scriptConfigureContent = script_configure_scroll_view.Find("Viewport/Content").GetComponent<RectTransform>();
+        detailConfigureContent = detail_configure_scroll_view.Find("Viewport/Content").GetComponent<RectTransform>();
+        
+        
     }
 }
